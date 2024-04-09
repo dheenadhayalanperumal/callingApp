a14103ffe2feda78a1e29e522e3f7865c3e12b93
dheenadhayalanperumal <dheena1992@gmail.com>
2024-04-09 16:40:29 +0530

    permission

---
+++ src/DashBoard.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+
+const DashBoard = () => {
+    const playerRef = useRef(null);
+
+    useEffect(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+            .then((stream) => {
+                if (playerRef.current) {
+                    playerRef.current.srcObject = stream;
+                    playerRef.current.play();
+                }
+            });
+    }, []);
+
+    return (
+        <div>
+            <div>
+                <Link to="/signup">Signup</Link>
+                <Link to="/signin">Signin</Link>
+            </div>
+            <div className="text-3xl">Dashboard page1</div>
+            <video autoPlay controls ref={playerRef} />
+        </div>
+    );
+}
+
+export default DashBoard;
