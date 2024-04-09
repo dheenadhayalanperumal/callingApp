2e0f81712c029483fdf394f826918f83118bc3a7
dheenadhayalanperumal <dheena1992@gmail.com>
2024-04-09 16:33:11 +0530

    permission

--- src/DashBoard.jsx
+++ src/DashBoard.jsx
@@ -20,6 +20,7 @@ const DashBoard = () => {
                 <Link to="/signup">Signup</Link>
                 <Link to="/signin">Signin</Link>
             </div>
+            <div className="text-3xl">Dashboard page</div>
             <video autoPlay controls ref={playerRef} />
         </div>
     );
